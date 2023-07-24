f48778a2d774a41a14a857377c04821057166840
mike-droid <miguelreyesmoreno@hotmail.com>
2023-07-24 17:03:26 -0600

    Clase 21 - relacion 1 a muchos en el servicio

--- src/products/services/products.service.ts
+++ src/products/services/products.service.ts
@@ -4,15 +4,19 @@ import { Repository } from 'typeorm';
 
 import { Product } from './../entities/product.entity';
 import { CreateProductDto, UpdateProductDto } from '../dtos/products.dto';
+import { BrandsService } from './brands.service';
 
 @Injectable()
 export class ProductsService {
   constructor(
     @InjectRepository(Product) private productRepo: Repository<Product>,
+    private brandService: BrandsService,
   ) {}
 
   async findAll() {
-    return await this.productRepo.find();
+    return await this.productRepo.find({
+      relations: ['brand'],
+    });
   }
 
   async findOne(id: number) {
@@ -25,11 +29,19 @@ export class ProductsService {
 
   async create(data: CreateProductDto) {
     const newProduct = this.productRepo.create(data);
+    if (data.brandId) {
+      const brand = await this.brandService.findOne(data.brandId);
+      newProduct.brand = brand;
+    }
     return await this.productRepo.save(newProduct);
   }
 
   async update(id: number, changes: UpdateProductDto) {
     const product = await this.productRepo.findOne(id);
+    if (changes.brandId) {
+      const brand = await this.brandService.findOne(changes.brandId);
+      product.brand = brand;
+    }
     this.productRepo.merge(product, changes);
     return this.productRepo.save(product);
   }
