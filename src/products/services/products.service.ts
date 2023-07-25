3e89ad0e153b7ffc90ee8da42281e58af071b067
mike-droid <miguelreyesmoreno@hotmail.com>
2023-07-25 14:08:33 -0600

    Clase 18 - muchos a muchos en el servicio

--- src/products/services/products.service.ts
+++ src/products/services/products.service.ts
@@ -2,15 +2,17 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
-import { Product } from './../entities/product.entity';
 import { CreateProductDto, UpdateProductDto } from '../dtos/products.dto';
-import { BrandsService } from './brands.service';
+import { Product } from './../entities/product.entity';
+import { Category } from '../entities/category.entity';
+import { Brand } from '../entities/brand.entity';
 
 @Injectable()
 export class ProductsService {
   constructor(
     @InjectRepository(Product) private productRepo: Repository<Product>,
-    private brandService: BrandsService,
+    @InjectRepository(Brand) private brandRepo: Repository<Brand>,
+    @InjectRepository(Category) private categoryRepo: Repository<Category>,
   ) {}
 
   async findAll() {
@@ -20,7 +22,9 @@ export class ProductsService {
   }
 
   async findOne(id: number) {
-    const product = await this.productRepo.findOne(id);
+    const product = await this.productRepo.findOne(id, {
+      relations: ['brand', 'categories'],
+    });
     if (!product) {
       throw new NotFoundException(`Product #${id} not found`);
     }
@@ -30,8 +34,13 @@ export class ProductsService {
   async create(data: CreateProductDto) {
     const newProduct = this.productRepo.create(data);
     if (data.brandId) {
-      const brand = await this.brandService.findOne(data.brandId);
+      const brand = await this.brandRepo.findOne(data.brandId);
       newProduct.brand = brand;
+    }
+
+    if (data.categoriesIds) {
+      const categories = await this.categoryRepo.findByIds(data.categoriesIds);
+      newProduct.categories = categories;
     }
     return await this.productRepo.save(newProduct);
   }
@@ -39,7 +48,7 @@ export class ProductsService {
   async update(id: number, changes: UpdateProductDto) {
     const product = await this.productRepo.findOne(id);
     if (changes.brandId) {
-      const brand = await this.brandService.findOne(changes.brandId);
+      const brand = await this.brandRepo.findOne(changes.brandId);
       product.brand = brand;
     }
     this.productRepo.merge(product, changes);
